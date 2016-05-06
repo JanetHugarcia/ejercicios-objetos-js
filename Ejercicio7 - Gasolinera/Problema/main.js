8b270043bd2283d7b527522cb0f9d8b394acefa9
JanetHugarcia <nayavz@gmail.com>
2016-05-06 18:11:49 -0500

    quitandole el bug al 7

--- Ejercicio7 - Gasolinera/Problema/main.js
+++ Ejercicio7 - Gasolinera/Problema/main.js
@@ -11,16 +11,25 @@ var bomba = {
 	}
 
 }
-while(bomba.cantidad>=0){
-	bomba.llevado = parseFloat(prompt("GASOLINERA\nGasolina disponible: "+ bomba.cantidad +"L" + "\nCosto por litro: "+ bomba.pagoxlitro.toFixed(2)+"\n¿Cuántos litros llevará?"));
-	alert("costo : " + bomba.costo().toFixed(2) + "\n" + "disponible : "+bomba.disponible()+ "L" + "\nGRACIAS POR SU COMPRA");
-	bomba.cantidad=bomba.disponible();
 
-	if(bomba.cantidad==0){
-		alert("Ya no hay litros disponibles");
+do{
+	bomba.llevado = parseFloat(prompt("GASOLINERA\nGasolina disponible: "+ bomba.cantidad +"L" + "\nCosto por litro: "+ bomba.pagoxlitro.toFixed(2)+"\n¿Cuántos litros llevará?"));
+
+	if(bomba.llevado<=bomba.cantidad){
+		alert("costo : " + bomba.costo().toFixed(2) + "\n" + "disponible : "+bomba.disponible()+ "L" + "\nGRACIAS POR SU COMPRA");
+
+		bomba.cantidad=bomba.disponible();
+
+		if(bomba.cantidad==0){
+			alert("Ya no hay litros disponibles");
+			break;
+		}
+	}else{
+		alert("Pides más de lo que puedo dar");
 		break;
 	}
-}
+		
+}while(bomba.cantidad>=0)
 
 
 
